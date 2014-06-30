36efed15f5967d4f999dd0e4ba8d2d71e072f302
David Arcus <david.arcus@colensobbdo.co.nz>
2014-06-30 16:50:08 +1200

    removed unneeded wordpress themes

--- themes/dave/single.php
+++ themes/dave/single.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * The Template for displaying all single posts
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
- */
-
 get_header(); ?>
 
 	<div id="primary" class="content-area">
